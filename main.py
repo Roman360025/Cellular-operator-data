d0593cca05fa3c607b6663c9e12bc8e4ce5626aa
Roman <professor360025@gmail.com>
2021-09-10 22:52:24 +0500

    Файл с функциями переделан в файл с классом и методами

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import os
-from multiprocessing import Pool
-from map_reduce_operator import chunkIt, reducer, chunks_mapper, name_of_directory
+from multiprocessing import Pool, Manager
+# from map_reduce_operator import chunkIt, reducer, chunks_mapper, name_of_directory
 import map_reduce_operator
 
 if __name__ == "__main__":
-    number_of_chunks = 2
+    number_of_chunks = 4
 
     pool = Pool(number_of_chunks)
-    data = sorted(os.listdir(name_of_directory))
-    data_chunks = chunkIt(data, number_of_chunks)
+    # manager = Manager()
+    operator = map_reduce_operator.H_operator()
+    data = sorted(os.listdir(operator.name_of_directory))
+
+    data_chunks = operator.chunkIt(data, number_of_chunks)
 
     name_of_h = {}
 
     for i in data_chunks:
         name_of_h[data_chunks.index(i)] = i[0].split('.')[1]
 
-    mapped = pool.map(chunks_mapper, data_chunks)
-    print(list(mapped))
+    mapped = pool.map(operator.chunks_mapper, data_chunks)
+    # print(list(mapped))
     print('Список средних скоростей')
 
     for i in list(mapped):
-        print(name_of_h[list(mapped).index(i)],  ":", i)
+        print(name_of_h[list(mapped).index(i)], ":", i)
 
     print()
-    reduce = reducer(list(mapped))
+    reduce = operator.reducer(list(mapped))
     print('Наивысшая средняя скорость:', name_of_h[reduce])
--- map_reduce_operator.py
+++ map_reduce_operator.py
@@ -1,72 +1,69 @@
 import csv
 
-h_max_speed = {}
-# name_of_directory = 'Assignment with a cellular operator/'
-name_of_directory = 'test/'
 
+class H_operator:
 
-def mapper(log_file):
-    log_file = name_of_directory + log_file
-    h = log_file.split('.')[1]
+    def __init__(self):
+        self.h_max_speed = {}
+        # self.name_of_directory = 'test/'
+        self.name_of_directory = 'test/'
+        self.name_of_directory = 'Assignment with a cellular operator/'
 
-    global h_max_speed
+    def mapper(self, log_file):
+        log_file = self.name_of_directory + log_file
+        h = log_file.split('.')[1]
 
-    if h not in h_max_speed:
-        h_max_speed[h] = 0
+        if h not in self.h_max_speed:
+            self.h_max_speed[h] = 0
 
-    with open(log_file, newline='') as File:
-        day = 1
-        average_in_day = []
-        reader = csv.reader(File, delimiter=',')
-        next(reader)  # Пропускаем первую строку
-        first_row = next(reader)
-        first_time, bytes = float(first_row[1][1:]), float(first_row[2][1:])  # Получаем первый отсчёт и количество байт
+        with open(log_file, newline='') as File:
+            day = 1
+            average_in_day = []
+            reader = csv.reader(File, delimiter=',')
+            next(reader)  # Пропускаем первую строку
+            first_row = next(reader)
+            first_time, bytes = float(first_row[1][1:]), float(
+                first_row[2][1:])  # Получаем первый отсчёт и количество байт
 
-        if bytes > h_max_speed[h]:
-            h_max_speed[h] = bytes
+            if bytes > self.h_max_speed[h]:
+                self.h_max_speed[h] = bytes
 
-        for row in reader:
-            if float(row[2][1:]) > h_max_speed[h]:
-                h_max_speed[h] = bytes
-            if int(row[0]) != day:
-                day = int(row[0])
-                average_in_day.append(bytes / (last_time - first_time))
-                first_time, bytes = float(row[1][1:]), float(row[2][1:])
-                continue
-            bytes += float(row[2][1:])
-            last_time = float(row[1][1:])
+            for row in reader:
+                if float(row[2][1:]) > self.h_max_speed[h]:
+                    self.h_max_speed[h] = bytes
+                if int(row[0]) != day:
+                    day = int(row[0])
+                    average_in_day.append(bytes / (last_time - first_time))
+                    first_time, bytes = float(row[1][1:]), float(row[2][1:])
+                    continue
+                bytes += float(row[2][1:])
+                last_time = float(row[1][1:])
 
-    average_in_day.append(bytes / (last_time - first_time))  # Учитываем последнюю строку
-    return sum(average_in_day) / 7
+        average_in_day.append(bytes / (last_time - first_time))  # Учитываем последнюю строку
 
+        self.init_speed(self.h_max_speed)
+        return sum(average_in_day) / 7
 
-def chunkIt(seq, num):
-    avg = len(seq) / float(num)
-    out = []
-    last = 0.0
+    def chunkIt(self, seq, num):
+        avg = len(seq) / float(num)
+        out = []
+        last = 0.0
 
-    while last < len(seq):
-        out.append(seq[int(last):int(last + avg)])
-        last += avg
+        while last < len(seq):
+            out.append(seq[int(last):int(last + avg)])
+            last += avg
 
-    return out
+        return out
 
+    def reducer(self, list):
+        return list.index(max(list))
 
-def reducer(list):
-    return list.index(max(list))
+    def chunks_mapper(self, chunk):
+        mapped_chunk = map(self.mapper, chunk)
+        return self.reducer_of_chunk(list(mapped_chunk))
 
+    def reducer_of_chunk(self, some_h):
+        return sum(some_h) / len(some_h)
 
-def chunks_mapper(chunk):
-    mapped_chunk = map(mapper, chunk)
-
-    return reducer_of_chunk(list(mapped_chunk))
-
-
-def reducer_of_chunk(some_h):
-    return sum(some_h) / len(some_h)
-    # print(some_h)
-    # sum_of_average_speed = 0
-    # for i in some_h:
-    #     sum_of_average_speed += i[0]
-    # return sum_of_average_speed / len(some_h)
-
+    def init_speed(self, speed):
+        self.h_max_speed = speed
